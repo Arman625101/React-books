812d019628a4da1ca1d54bc808f72a6afb033ac8
Arman625101 <vip.arman2011@gmail.com>
2021-08-09 00:57:36 +0400

    modal for creating working, create logic working, edit modal and delete logic in progress

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "../../utils/Routes";
 import Navbar from "../Navbar/Navbar";
 import Modal from "../Modal/Modal";
+import { useFetchForSelect } from "../../utils/useFetch";
 import "./App.scss";
 
 function App() {
+  const [toRefresh, setToRefresh] = useState(false);
+  const [genres, setGenres] = useState([]);
+  const [authors, setAuthors] = useState([]);
   const [isShowModal, setIsShowModal] = useState(false);
-  // const setPathname = (pathname) => {
-  //   console.log("**", pathname);
-  // };
+  const [mode, setMode] = useState("");
+
+  const { data } = useFetchForSelect(toRefresh);
+
+  useEffect(() => {
+    console.log("EFFECT", data);
+    if (data) {
+      setGenres(data.genres);
+      setAuthors(data.authors);
+    }
+  }, [data, toRefresh]);
+
+  const handleRefresh = () => {
+    setToRefresh(true);
+    setIsShowModal(false);
+  };
 
   return (
     <Router>
       <div className="App">
         <Navbar />
         <div className="content">
-          <button onClick={() => setIsShowModal(true)}>Add new</button>
-          <Routes />
-          {/* setPathname={setPathname} */}
+          <button
+            onClick={() => {
+              setIsShowModal(true);
+              setToRefresh(false);
+              setMode("");
+            }}
+          >
+            Add new
+          </button>
+          <Routes toRefresh={toRefresh} />
         </div>
       </div>
-      {isShowModal && <Modal handleCloseModal={() => setIsShowModal(false)} />}
+      {isShowModal && (
+        <Modal
+          authors={authors}
+          genres={genres}
+          handleRefresh={handleRefresh}
+          mode={mode}
+          handleCloseModal={() => setIsShowModal(false)}
+        />
+      )}
     </Router>
   );
 }
--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -1,14 +1,41 @@
 import { useState, useEffect } from "react";
 
-const useFetch = (url) => {
+export const useFetchForSelect = (refresh) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
+  const url = "http://localhost:8000";
+
+  useEffect(() => {
+    Promise.all([fetch(`${url}/genres`), fetch(`${url}/authors`)])
+      .then((res) => Promise.all(res.map((res) => res.json())))
+      .then((data) => {
+        setData({ genres: data[0], authors: data[1] });
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("fetch aborted");
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
+      });
+  }, [refresh]);
+  return { data, isPending, error };
+};
+
+export const useFetch = (path, refresh) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  const url = "http://localhost:8000";
 
   useEffect(() => {
     const abortCont = new AbortController();
 
-    fetch(url)
+    fetch(`${url + path}`, { signal: abortCont.signal })
       .then((res) => {
         if (!res.ok) {
           throw Error("could not fetch the data");
@@ -31,8 +58,49 @@ const useFetch = (url) => {
       });
 
     return () => abortCont.abort();
-  }, [url]);
+  }, [path, refresh]);
   return { data, isPending, error };
 };
 
-export default useFetch;
+export const usePost = async (path, body) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  const url = "http://localhost:8000";
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+    if (typeof body === "string" && body.length <= 0) {
+      return () => abortCont.abort();
+    }
+    fetch(`${url + path}`, {
+      method: "POST",
+      headers: { "Content-Type": "Application/json" },
+      body,
+      signal: abortCont.signal,
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+        abortCont.abort();
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("fetch aborted");
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
+      });
+    return () => abortCont.abort();
+  }, [path, body]);
+
+  return { data, isPending, error };
+};
